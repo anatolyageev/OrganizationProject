7d55a66f3d3f4168bc76531c56a385866c766654
Anatolii <anatol1812@gmail.com>
2019-05-27 23:03:31 +0300

    Corrections was done according to comments

--- src/main/java/ua/su/OrganizationsProjectMain.java
+++ src/main/java/ua/su/OrganizationsProjectMain.java
@@ -3,7 +3,6 @@ package ua.su;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class OrganizationsProjectMain {
   public static void main(final String[] args) {
